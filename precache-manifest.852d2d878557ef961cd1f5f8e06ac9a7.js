3284386e2c217c2d0333ef6ab06896767b0a14be
herongxhr <herongxhr@163.com>
2019-01-20 20:55:46 +0800

    Updates

---
+++ precache-manifest.852d2d878557ef961cd1f5f8e06ac9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/myapp/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "a3f8d049f1f6347200ab",
+    "url": "/myapp/static/js/main.a3f8d049.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/myapp/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a3f8d049f1f6347200ab",
+    "url": "/myapp/static/css/main.fc417ec3.chunk.css"
+  },
+  {
+    "revision": "c5ac7c505b31f534a6cfbe1a33975490",
+    "url": "/myapp/index.html"
+  }
+];
